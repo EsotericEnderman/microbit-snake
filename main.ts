c017e7d4ab932fb34b84b8ec5d7349d27744545e
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-26 14:56:57 +0100

    Generate the `Snake`'s starting location

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+const minimumX = 0;
+const minimumY = 0;
+
+const maximumX = 4;
+const maximumY = 4;
+
 type LinkedListElement<T> = {
   value: T;
   next: LinkedListElement<T> | null;
@@ -7,15 +13,33 @@ type LinkedList<T> = {
     head: LinkedListElement<T>;
 }
 
-type Vector = {
+class Vector {
     x: number;
     y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static getRandomVector(minimumX: number, minimumY: number, maximumX: number, maximumY: number) {
+        const x = Math.floor(minimumX + Math.random() * (maximumX - minimumX));
+        const y = Math.floor(minimumY + Math.random() * (maximumY - minimumY));
+
+        return new Vector(x, y);
+    }
 }
 
 type SnakePart = Vector;
 
 class Snake {
+    direction: Vector;
+
     parts: LinkedList<SnakePart>;
+
+    public static generateRandomParts(count: number) {
+        const startingLocation = Vector.getRandomVector(minimumX, minimumY, maximumX, maximumY);
+    }
 }
 
 const snake = new Snake();
