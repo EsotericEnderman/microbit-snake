9a5dcaceebf7e285b36aa03443b4c13999303113
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-28 03:08:07 +0100

    Allow no `interval` to be used

--- random.ts
+++ random.ts
@@ -1,5 +1,9 @@
-function random(minimum: number, maximum: number, interval: number) {
+function random(minimum: number, maximum: number, interval?: number) {
     const intervalRandomNumber = Math.random() * (minimum - maximum);
+
+    if (interval) {
+        return minimum + intervalRandomNumber;
+    }
 
     const rounded = round(intervalRandomNumber, interval);
     const clamped = Math.max(rounded, rounded - interval);
