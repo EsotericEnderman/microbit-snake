e729285c19e380fad57c5c1327e0aa9899372468
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-28 01:59:52 +0100

    Expand `Vector` class

--- main.ts
+++ main.ts
@@ -59,9 +59,33 @@ class Vector {
     x: number;
     y: number;
 
+    public static readonly north = new Vector(0, 1);
+    public static readonly east = new Vector(1, 0);
+    public static readonly south = new Vector(0, -1);
+    public static readonly west = new Vector(-1, 0);
+
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
+    }
+
+    public get magnitude() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public rotate(angle: number) {
+        const currentAngle = Math.atan2(this.y, this.x);
+        const magnitude = this.magnitude;
+
+        const newAngle = currentAngle + angle;
+        this.x = Math.cos(newAngle) * magnitude;
+        this.y = Math.sin(newAngle) * magnitude;
+
+        return this;
+    }
+
+    public clone() {
+        return new Vector(this.x, this.y);
     }
 
     public static getRandomVector(minimumX: number, minimumY: number, maximumX: number, maximumY: number) {
