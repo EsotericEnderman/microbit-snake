0dc383844a6f5d7ad2424298d6517cff270c9624
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-28 13:40:51 +0100

    Add `Rectangle` class

---
+++ Rectangle.ts
@@ -0,0 +1,13 @@
+class Rectangle {
+    topLeft: Vector;
+
+    width: number;
+    height: number;
+
+    constructor(topLeft: Vector, width: number, height: number) {
+        this.topLeft = topLeft;
+        
+        this.width = width;
+        this.height = height;
+    }
+}
