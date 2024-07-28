befc2f65a28251af71c1279ccff005a97a7babbd
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-28 13:49:32 +0100

    Add useful methods

--- Rectangle.ts
+++ Rectangle.ts
@@ -10,4 +10,26 @@ class Rectangle {
         this.width = width;
         this.height = height;
     }
+
+    public get maximumX() {
+        return this.topLeft.x + this.width;
+    }
+
+    public get maximumY() {
+        return this.topLeft.y + this.height;
+    }
+
+    public get minimumX() {
+        return this.topLeft.x;
+    }
+
+    public get minimumY() {
+        return this.topLeft.y;
+    }
+
+    public includes(position: Vector) {
+        const { x, y } = position;
+
+        return x > this.minimumX && x < this.maximumX && y > this.minimumY && y < this.maximumY;
+    }
 }
