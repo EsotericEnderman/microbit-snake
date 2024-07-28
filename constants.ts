6e04d97708095d3827a8ef0435614d6e69754078
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-28 13:43:10 +0100

    Add `ledSquare` variable

--- constants.ts
+++ constants.ts
@@ -8,3 +8,5 @@ const degreesToRadians = Math.PI / 180;
 const radiansToDegrees = 1 / degreesToRadians;
 
 const startingSnakeLength = 2;
+
+const ledSquare = new Square(new Vector(minimumX, minimumY), maximumX - minimumX + 1);
