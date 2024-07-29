1cc78ddc360318b888b96ec698f9a79eb68e5b2b
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-29 14:28:21 +0100

    Add collectible system

--- main.ts
+++ main.ts
@@ -2,3 +2,34 @@ const snake = new Snake();
 
 input.onButtonPressed(Button.A, () => snake.direction.rotate(-90 * degreesToRadians).round());
 input.onButtonPressed(Button.B, () => snake.direction.rotate(90 * degreesToRadians).round());
+
+class Collectible {
+
+    public static instance: Collectible;
+
+    public readonly position: Vector;
+
+    private constructor(position: Vector) {
+        if (Collectible.instance) {
+            Collectible.instance.despawn();
+        }
+
+        this.position = position;
+
+        led.plot(position.x, position.y);
+    }
+
+    public static initialise() {
+        Collectible.instance = Collectible.getRandomCollectible();
+    }
+
+    public despawn() {
+        led.unplot(this.position.x, this.position.y);
+    }
+
+    public static getRandomCollectible() {
+        return new Collectible(Vector.getRandomVector(minimumX, minimumY, maximumX, maximumY));
+    }
+}
+
+Collectible.initialise();
