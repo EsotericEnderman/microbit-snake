252bdc7dacdeaa459f2724955219c50d0f6aed68
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-29 13:48:00 +0100

    Add turning

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
 const snake = new Snake();
+
+input.onButtonPressed(Button.A, () => snake.direction.rotate(-90 * degreesToRadians).round());
+input.onButtonPressed(Button.B, () => snake.direction.rotate(90 * degreesToRadians).round());
